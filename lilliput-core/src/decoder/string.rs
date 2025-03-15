0ef83faa47420dc91f4a6cafc30ee32f18b61023
Vincent Esche <regexident@gmail.com>
2025-03-15 14:52:23 +0100

    Extract encoding logic for string values into dedicated `struct StringEncoder`

---
+++ lilliput-core/src/decoder/string.rs
@@ -0,0 +1,52 @@
+use crate::value::{StringValue, ValueType};
+
+use super::{Decoder, DecoderError};
+
+#[derive(Debug)]
+pub struct StringDecoder<'a, 'de> {
+    inner: &'de mut Decoder<'a>,
+}
+
+impl<'a, 'de> StringDecoder<'a, 'de> {
+    pub(super) fn with(inner: &'de mut Decoder<'a>) -> Self {
+        Self { inner }
+    }
+
+    pub(super) fn decode_string(&mut self) -> Result<String, DecoderError> {
+        let byte = self.inner.pull_byte_expecting_type(ValueType::String)?;
+
+        let is_long = byte & StringValue::VARIANT_BIT != 0b0;
+
+        if is_long {
+            let is_valid = byte & StringValue::LONG_RESERVED_BITS == 0b0;
+            let len_width = (byte & StringValue::LONG_LEN_WIDTH_BITS) as usize + 1;
+
+            assert!(is_valid, "padding bits should be zero");
+
+            let mut bytes: [u8; 8] = [0b0; 8];
+
+            let range = {
+                let start = 8 - len_width;
+                let end = start + len_width;
+                start..end
+            };
+
+            bytes[range].copy_from_slice(self.inner.pull_bytes(len_width)?);
+
+            let len = u64::from_be_bytes(bytes) as usize;
+
+            let mut bytes = Vec::with_capacity(len.min(self.inner.remaining_len()));
+            bytes.extend_from_slice(self.inner.pull_bytes(len)?);
+
+            let value = String::from_utf8(bytes)?;
+
+            Ok(value)
+        } else {
+            Err(DecoderError::IncompatibleProfile)
+        }
+    }
+
+    pub(super) fn decode_string_value(&mut self) -> Result<StringValue, DecoderError> {
+        self.decode_string().map(From::from)
+    }
+}
---
+++ lilliput-core/src/encoder/string.rs
@@ -0,0 +1,41 @@
+use crate::value::StringValue;
+
+use super::{Encoder, EncoderError};
+
+#[derive(Debug)]
+pub(super) struct StringEncoder<'en> {
+    inner: &'en mut Encoder,
+}
+
+impl<'en> StringEncoder<'en> {
+    pub(super) fn with(inner: &'en mut Encoder) -> Self {
+        Self { inner }
+    }
+
+    pub(super) fn encode_string(&mut self, value: &str) -> Result<(), EncoderError> {
+        let value: &str = value;
+
+        // Push the value's metadata:
+        let mut head_byte = StringValue::PREFIX_BIT;
+        head_byte |= StringValue::VARIANT_BIT;
+
+        head_byte |= 8 - 1; // width, minus 1
+        self.inner.push_byte(head_byte)?;
+
+        // Push the value's length:
+        let neck_bytes = value.len().to_be_bytes();
+        self.inner.push_bytes(&neck_bytes)?;
+
+        // Push the value's actual bytes:
+        let tail_bytes = value.as_bytes();
+        self.inner.push_bytes(tail_bytes)?;
+
+        Ok(())
+    }
+
+    pub(super) fn encode_string_value(&mut self, value: &StringValue) -> Result<(), EncoderError> {
+        self.encode_string(&value.0)?;
+
+        Ok(())
+    }
+}
