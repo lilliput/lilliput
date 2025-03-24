bb884b49f0a4d2d2cc287c6c28ab65b31c357bb1
Vincent Esche <regexident@gmail.com>
2025-03-24 20:58:37 +0100

    Add "lilliput-serde" crate

---
+++ lilliput-serde/src/ser.rs
@@ -0,0 +1,388 @@
+use lilliput_core::{
+    config::EncodingConfig,
+    encoder::Encoder,
+    io::{StdIoWriter, Write},
+};
+use serde::{ser, Serialize};
+
+use crate::{Error, Result};
+
+pub struct Serializer<W> {
+    pub(crate) encoder: Encoder<W>,
+}
+
+impl<W> Serializer<W> {
+    pub fn from_writer(writer: W, config: EncodingConfig) -> Self {
+        Self {
+            encoder: Encoder::new(writer, config),
+        }
+    }
+}
+
+pub fn to_vec<T>(value: &T) -> Result<Vec<u8>>
+where
+    T: ?Sized + Serialize,
+{
+    let mut vec: Vec<u8> = Vec::new();
+    let writer = StdIoWriter::new(&mut vec);
+    let config = EncodingConfig::default();
+    let mut serializer = Serializer::from_writer(writer, config);
+
+    value.serialize(&mut serializer)?;
+
+    Ok(vec)
+}
+
+#[cfg(feature = "std")]
+pub fn to_writer<W, T>(writer: W, value: &T) -> Result<()>
+where
+    W: std::io::Write,
+    T: ?Sized + Serialize,
+{
+    let config = EncodingConfig::default();
+    let mut serializer = Serializer::from_writer(StdIoWriter::new(writer), config);
+
+    value.serialize(&mut serializer)
+}
+
+impl<W> ser::Serializer for &mut Serializer<W>
+where
+    W: Write,
+{
+    type Ok = ();
+    type Error = Error;
+
+    type SerializeSeq = Self;
+    type SerializeTuple = Self;
+    type SerializeTupleStruct = Self;
+    type SerializeTupleVariant = Self;
+    type SerializeMap = Self;
+    type SerializeStruct = Self;
+    type SerializeStructVariant = Self;
+
+    fn serialize_bool(self, value: bool) -> Result<()> {
+        self.encoder.encode_bool(value)
+    }
+
+    fn serialize_i8(self, value: i8) -> Result<()> {
+        self.encoder.encode_i64(value.into())
+    }
+
+    fn serialize_i16(self, value: i16) -> Result<()> {
+        self.encoder.encode_i64(value.into())
+    }
+
+    fn serialize_i32(self, value: i32) -> Result<()> {
+        self.encoder.encode_i64(value.into())
+    }
+
+    fn serialize_i64(self, value: i64) -> Result<()> {
+        self.encoder.encode_i64(value)
+    }
+
+    fn serialize_u8(self, value: u8) -> Result<()> {
+        self.encoder.encode_u64(value.into())
+    }
+
+    fn serialize_u16(self, value: u16) -> Result<()> {
+        self.encoder.encode_u64(value.into())
+    }
+
+    fn serialize_u32(self, value: u32) -> Result<()> {
+        self.encoder.encode_u64(value.into())
+    }
+
+    fn serialize_u64(self, value: u64) -> Result<()> {
+        self.encoder.encode_u64(value)
+    }
+
+    fn serialize_f32(self, value: f32) -> Result<()> {
+        self.encoder.encode_f32(value)
+    }
+
+    fn serialize_f64(self, value: f64) -> Result<()> {
+        self.encoder.encode_f64(value)
+    }
+
+    fn serialize_char(self, value: char) -> Result<()> {
+        self.serialize_str(&value.to_string())
+    }
+
+    fn serialize_str(self, value: &str) -> Result<()> {
+        self.encoder.encode_str(value)
+    }
+
+    fn serialize_bytes(self, value: &[u8]) -> Result<()> {
+        self.encoder.encode_bytes(value)
+    }
+
+    fn serialize_none(self) -> Result<()> {
+        self.encoder.encode_null()
+    }
+
+    fn serialize_some<T>(self, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        value.serialize(self)
+    }
+
+    fn serialize_unit(self) -> Result<()> {
+        self.encoder.encode_unit()
+    }
+
+    fn serialize_unit_struct(self, _name: &'static str) -> Result<()> {
+        self.serialize_unit()
+    }
+
+    fn serialize_unit_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        variant: &'static str,
+    ) -> Result<()> {
+        self.serialize_str(variant)
+    }
+
+    fn serialize_newtype_struct<T>(self, _name: &'static str, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        value.serialize(self)
+    }
+
+    fn serialize_newtype_variant<T>(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        variant: &'static str,
+        value: &T,
+    ) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        let header = self.encoder.header_for_map(1);
+        self.encoder.encode_map_header(&header)?;
+
+        variant.serialize(&mut *self)?;
+        value.serialize(&mut *self)?;
+
+        Ok(())
+    }
+
+    fn serialize_seq(self, len: Option<usize>) -> Result<Self::SerializeSeq> {
+        let Some(len) = len else {
+            return Err(Error::unknown_length());
+        };
+
+        let header = self.encoder.header_for_seq(len);
+        self.encoder.encode_seq_header(&header)?;
+
+        Ok(self)
+    }
+
+    fn serialize_tuple(self, len: usize) -> Result<Self::SerializeTuple> {
+        self.serialize_seq(Some(len))
+    }
+
+    fn serialize_tuple_struct(
+        self,
+        _name: &'static str,
+        len: usize,
+    ) -> Result<Self::SerializeTupleStruct> {
+        self.serialize_seq(Some(len))
+    }
+
+    fn serialize_tuple_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        variant: &'static str,
+        len: usize,
+    ) -> Result<Self::SerializeTupleVariant> {
+        let outer_map_header = self.encoder.header_for_map(1);
+        self.encoder.encode_map_header(&outer_map_header)?;
+
+        variant.serialize(&mut *self)?;
+
+        let inner_seq_header = self.encoder.header_for_seq(len);
+        self.encoder.encode_seq_header(&inner_seq_header)?;
+
+        Ok(self)
+    }
+
+    fn serialize_map(self, len: Option<usize>) -> Result<Self::SerializeMap> {
+        let Some(len) = len else {
+            return Err(Error::unknown_length());
+        };
+
+        let header = self.encoder.header_for_map(len);
+        self.encoder.encode_map_header(&header)?;
+
+        Ok(self)
+    }
+
+    fn serialize_struct(self, _name: &'static str, len: usize) -> Result<Self::SerializeStruct> {
+        self.serialize_map(Some(len))
+    }
+
+    fn serialize_struct_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        variant: &'static str,
+        len: usize,
+    ) -> Result<Self::SerializeStructVariant> {
+        let outer_map_header = self.encoder.header_for_map(1);
+        self.encoder.encode_map_header(&outer_map_header)?;
+
+        variant.serialize(&mut *self)?;
+
+        let inner_map_header = self.encoder.header_for_map(len);
+        self.encoder.encode_map_header(&inner_map_header)?;
+
+        Ok(self)
+    }
+}
+
+impl<W> ser::SerializeSeq for &mut Serializer<W>
+where
+    W: Write,
+{
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_element<T>(&mut self, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        value.serialize(&mut **self)
+    }
+
+    fn end(self) -> Result<()> {
+        Ok(())
+    }
+}
+
+impl<W> ser::SerializeTuple for &mut Serializer<W>
+where
+    W: Write,
+{
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_element<T>(&mut self, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        value.serialize(&mut **self)
+    }
+
+    fn end(self) -> Result<()> {
+        Ok(())
+    }
+}
+
+impl<W> ser::SerializeTupleStruct for &mut Serializer<W>
+where
+    W: Write,
+{
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T>(&mut self, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        value.serialize(&mut **self)
+    }
+
+    fn end(self) -> Result<()> {
+        Ok(())
+    }
+}
+
+impl<W> ser::SerializeTupleVariant for &mut Serializer<W>
+where
+    W: Write,
+{
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T>(&mut self, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        value.serialize(&mut **self)
+    }
+
+    fn end(self) -> Result<()> {
+        Ok(())
+    }
+}
+
+impl<W> ser::SerializeMap for &mut Serializer<W>
+where
+    W: Write,
+{
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_key<T>(&mut self, key: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        key.serialize(&mut **self)
+    }
+
+    fn serialize_value<T>(&mut self, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        value.serialize(&mut **self)
+    }
+
+    fn end(self) -> Result<()> {
+        Ok(())
+    }
+}
+
+impl<W> ser::SerializeStruct for &mut Serializer<W>
+where
+    W: Write,
+{
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T>(&mut self, key: &'static str, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        key.serialize(&mut **self)?;
+        value.serialize(&mut **self)
+    }
+
+    fn end(self) -> Result<()> {
+        Ok(())
+    }
+}
+
+impl<W> ser::SerializeStructVariant for &mut Serializer<W>
+where
+    W: Write,
+{
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T>(&mut self, key: &'static str, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        key.serialize(&mut **self)?;
+        value.serialize(&mut **self)
+    }
+
+    fn end(self) -> Result<()> {
+        Ok(())
+    }
+}
---
+++ lilliput-serde/src/tests.rs
@@ -0,0 +1,314 @@
+use std::collections::BTreeMap;
+
+use proptest::prelude::*;
+
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+
+use crate::{from_slice, to_vec, value::*, Error, Value};
+
+#[cfg_attr(test, derive(proptest_derive::Arbitrary))]
+#[derive(Default, Copy, Clone, Eq, PartialEq, Debug, Serialize, Deserialize)]
+struct UnitStruct;
+
+#[cfg_attr(test, derive(proptest_derive::Arbitrary))]
+#[derive(Default, Copy, Clone, Eq, PartialEq, Debug, Serialize, Deserialize)]
+struct NewtypeStruct<T>(pub T);
+
+#[cfg_attr(test, derive(proptest_derive::Arbitrary))]
+#[derive(Default, Copy, Clone, Eq, PartialEq, Debug, Serialize, Deserialize)]
+struct TupleStruct<T>(pub T, pub T);
+
+#[cfg_attr(test, derive(proptest_derive::Arbitrary))]
+#[derive(Default, Copy, Clone, Eq, PartialEq, Debug, Serialize, Deserialize)]
+struct Struct<T> {
+    pub a: T,
+    pub b: T,
+}
+
+#[cfg_attr(test, derive(proptest_derive::Arbitrary))]
+#[derive(Default, Copy, Clone, Eq, PartialEq, Debug, Serialize, Deserialize)]
+enum Enum<T> {
+    #[default]
+    UnitVariant,
+    NewtypeTupleVariant(T),
+    TupleVariant(T, T),
+    NewtypeStructVariant {
+        a: T,
+    },
+    StructVariant {
+        a: T,
+        b: T,
+    },
+}
+
+impl<T> Enum<T>
+where
+    T: Arbitrary + std::fmt::Debug,
+{
+    fn arbitrary_unit_variant() -> impl Strategy<Value = Self> {
+        <()>::arbitrary().prop_map(|_| Self::UnitVariant)
+    }
+
+    fn arbitrary_newtype_variant() -> impl Strategy<Value = Self> {
+        prop_oneof![
+            T::arbitrary().prop_map(|a| Self::NewtypeTupleVariant(a)),
+            T::arbitrary().prop_map(|a| Self::NewtypeStructVariant { a }),
+        ]
+    }
+
+    fn arbitrary_tuple_variant() -> impl Strategy<Value = Self> {
+        <(T, T)>::arbitrary().prop_map(|(a, b)| Self::TupleVariant(a, b))
+    }
+
+    fn arbitrary_struct_variant() -> impl Strategy<Value = Self> {
+        <(T, T)>::arbitrary().prop_map(|(a, b)| Self::StructVariant { a, b })
+    }
+}
+
+fn roundtrip<'de, T>(value: &T) -> Result<T, Error>
+where
+    T: Serialize + DeserializeOwned,
+{
+    let encoded: Vec<u8> = to_vec(&value).unwrap();
+    let decoded: T = from_slice(&encoded).unwrap();
+
+    Ok(decoded)
+}
+
+mod value {
+    use super::*;
+
+    proptest! {
+        #[test]
+        fn int_roundtrip(value in IntValue::arbitrary()) {
+            let decoded = roundtrip(&value)?;
+            prop_assert_eq!(&decoded, &value);
+        }
+
+        #[test]
+        fn string_roundtrip(value in StringValue::arbitrary()) {
+            let decoded = roundtrip(&value)?;
+            prop_assert_eq!(&decoded, &value);
+        }
+
+        #[test]
+        fn seq_roundtrip(value in SeqValue::arbitrary()) {
+            let decoded = roundtrip(&value)?;
+            prop_assert_eq!(&decoded, &value);
+        }
+
+        #[test]
+        fn map_roundtrip(value in MapValue::arbitrary()) {
+            let decoded = roundtrip(&value)?;
+            prop_assert_eq!(&decoded, &value);
+        }
+
+        #[test]
+        fn float_roundtrip(value in FloatValue::arbitrary()) {
+            let decoded = roundtrip(&value)?;
+            prop_assert_eq!(&decoded, &value);
+        }
+
+        #[test]
+        fn bytes_roundtrip(value in BytesValue::arbitrary()) {
+            let decoded = roundtrip(&value)?;
+            prop_assert_eq!(&decoded, &value);
+        }
+
+        #[test]
+        fn bool_roundtrip(value in BoolValue::arbitrary()) {
+            let decoded = roundtrip(&value)?;
+            prop_assert_eq!(&decoded, &value);
+        }
+
+        #[test]
+        fn null_roundtrip(value in NullValue::arbitrary()) {
+            let decoded = roundtrip(&value)?;
+            prop_assert_eq!(&decoded, &value);
+        }
+
+        #[test]
+        fn any_roundtrip(value in Value::arbitrary()) {
+            let encoded = to_vec(&value).unwrap();
+
+            let decoded = roundtrip(&value)?;
+            match (&decoded, &value) {
+                (Value::Int(lhs), Value::Int(rhs)) => assert_eq!(lhs, rhs),
+                (Value::String(lhs), Value::String(rhs)) => assert_eq!(lhs, rhs),
+                (Value::Seq(lhs), Value::Seq(rhs)) => assert_eq!(lhs, rhs),
+                (Value::Map(lhs), Value::Map(rhs)) => assert_eq!(lhs, rhs),
+                (Value::Float(lhs), Value::Float(rhs)) => assert_eq!(lhs, rhs),
+                (Value::Bytes(lhs), Value::Bytes(rhs)) => assert_eq!(lhs, rhs),
+                (Value::Bool(lhs), Value::Bool(rhs)) => assert_eq!(lhs, rhs),
+                (Value::Null(lhs), Value::Null(rhs)) => assert_eq!(lhs, rhs),
+                (lhs, rhs) => panic!("{lhs:#?} != {rhs:#?} (encoded: {encoded:?}"),
+            }
+            assert_eq!(&decoded, &value);
+            prop_assert_eq!(&decoded, &value);
+        }
+    }
+}
+
+proptest! {
+    #[test]
+    fn i8_roundtrip(value in i8::arbitrary()) {
+        let decoded = roundtrip(&value)?;
+        prop_assert_eq!(&decoded, &value);
+    }
+
+    #[test]
+    fn i16_roundtrip(value in i16::arbitrary()) {
+        let decoded = roundtrip(&value)?;
+        prop_assert_eq!(&decoded, &value);
+    }
+
+    #[test]
+    fn i32_roundtrip(value in i32::arbitrary()) {
+        let decoded = roundtrip(&value)?;
+        prop_assert_eq!(&decoded, &value);
+    }
+
+    #[test]
+    fn i64_roundtrip(value in i64::arbitrary()) {
+        let decoded = roundtrip(&value)?;
+        prop_assert_eq!(&decoded, &value);
+    }
+
+    #[test]
+    fn u8_roundtrip(value in u8::arbitrary()) {
+        let decoded = roundtrip(&value)?;
+        prop_assert_eq!(&decoded, &value);
+    }
+
+    #[test]
+    fn u16_roundtrip(value in u16::arbitrary()) {
+        let decoded = roundtrip(&value)?;
+        prop_assert_eq!(&decoded, &value);
+    }
+
+    #[test]
+    fn u32_roundtrip(value in u32::arbitrary()) {
+        let decoded = roundtrip(&value)?;
+        prop_assert_eq!(&decoded, &value);
+    }
+
+    #[test]
+    fn u64_roundtrip(value in u64::arbitrary()) {
+        let decoded = roundtrip(&value)?;
+        prop_assert_eq!(&decoded, &value);
+    }
+
+    #[test]
+    fn f32_roundtrip(value in f32::arbitrary()) {
+        let decoded = roundtrip(&value)?;
+        prop_assert_eq!(&decoded, &value);
+    }
+
+    #[test]
+    fn f64_roundtrip(value in f64::arbitrary()) {
+        let decoded = roundtrip(&value)?;
+        prop_assert_eq!(&decoded, &value);
+    }
+
+    #[test]
+    fn char_roundtrip(value in char::arbitrary()) {
+        let decoded = roundtrip(&value)?;
+        prop_assert_eq!(&decoded, &value);
+    }
+
+    #[test]
+    fn str_roundtrip(value in String::arbitrary()) {
+        let decoded = roundtrip(&value)?;
+        prop_assert_eq!(&decoded, &value);
+    }
+
+    #[test]
+    fn bytes_roundtrip(value in Vec::<u8>::arbitrary()) {
+        let decoded = roundtrip(&value)?;
+        prop_assert_eq!(&decoded, &value);
+    }
+
+    #[test]
+    fn none_roundtrip(_value in UnitStruct::arbitrary()) {
+        let decoded = roundtrip(&Option::<UnitStruct>::None)?;
+        prop_assert_eq!(&decoded, &None);
+    }
+
+    #[test]
+    fn some_roundtrip(value in UnitStruct::arbitrary()) {
+        let decoded = roundtrip(&Some(value))?;
+        prop_assert_eq!(&decoded, &Some(value));
+    }
+
+    #[test]
+    fn unit_roundtrip(_value in <()>::arbitrary()) {
+        roundtrip(&())?;
+        prop_assert_eq!(&(), &());
+    }
+
+    #[test]
+    fn unit_struct_roundtrip(value in UnitStruct::arbitrary()) {
+        let decoded = roundtrip(&value)?;
+        prop_assert_eq!(&decoded, &value);
+    }
+
+    #[test]
+    fn unit_variant_roundtrip(value in Enum::<bool>::arbitrary_unit_variant()) {
+        let decoded = roundtrip(&value)?;
+        prop_assert_eq!(&decoded, &value);
+    }
+
+    #[test]
+    fn newtype_struct_roundtrip(value in NewtypeStruct::<bool>::arbitrary()) {
+        let decoded = roundtrip(&value)?;
+        prop_assert_eq!(&decoded, &value);
+    }
+
+    #[test]
+    fn newtype_variant_roundtrip(value in Enum::<bool>::arbitrary_newtype_variant()) {
+        let decoded = roundtrip(&value)?;
+        prop_assert_eq!(&decoded, &value);
+    }
+
+    #[test]
+    fn seq_roundtrip(value in Vec::<bool>::arbitrary()) {
+        let decoded = roundtrip(&value)?;
+        prop_assert_eq!(&decoded, &value);
+    }
+
+    #[test]
+    fn tuple_roundtrip(value in (u8::arbitrary(), i8::arbitrary())) {
+        let decoded = roundtrip(&value)?;
+        prop_assert_eq!(&decoded, &value);
+    }
+
+    #[test]
+    fn tuple_struct_roundtrip(value in (u8::arbitrary(), i8::arbitrary())) {
+        let decoded = roundtrip(&value)?;
+        prop_assert_eq!(&decoded, &value);
+    }
+
+    #[test]
+    fn tuple_variant_roundtrip(value in Enum::<bool>::arbitrary_tuple_variant()) {
+        let decoded = roundtrip(&value)?;
+        prop_assert_eq!(&decoded, &value);
+    }
+
+    #[test]
+    fn map_roundtrip(value in BTreeMap::<bool, bool>::arbitrary()) {
+        let decoded = roundtrip(&value)?;
+        prop_assert_eq!(&decoded, &value);
+    }
+
+    #[test]
+    fn struct_roundtrip(value in Struct::<bool>::arbitrary()) {
+        let decoded = roundtrip(&value)?;
+        prop_assert_eq!(&decoded, &value);
+    }
+
+    #[test]
+    fn struct_variant_roundtrip(value in Enum::<bool>::arbitrary_struct_variant()) {
+        let decoded = roundtrip(&value)?;
+        prop_assert_eq!(&decoded, &value);
+    }
+}
