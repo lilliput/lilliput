7fe3fd17efec0ad063b27709c118abb966c98678
Vincent Esche <regexident@gmail.com>
2025-04-09 21:51:30 +0200

    Add functions for formatting bytes

--- lilliput-core/src/binary.rs
+++ lilliput-core/src/binary.rs
@@ -13,3 +13,15 @@ pub(crate) fn bits_if(bits: u8, condition: bool) -> u8 {
 pub(crate) fn all_bits_if(condition: bool) -> u8 {
     !(condition as u8).wrapping_sub(1)
 }
+
+#[allow(dead_code)]
+pub(crate) fn fmt_byte(byte: u8) -> String {
+    format!("{byte:08b}")
+}
+
+#[allow(dead_code)]
+pub(crate) fn fmt_bytes(bytes: &[u8]) -> String {
+    let vec: Vec<_> = bytes.iter().map(|b| format!("{b:08b}")).collect();
+
+    format!("{vec:?}")
+}
