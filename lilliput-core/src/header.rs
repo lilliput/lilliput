71985e40be24464410fc1848f8a31205420988dd
Vincent Esche <regexident@gmail.com>
2025-04-09 21:27:41 +0200

    Add tests for `Header` coding logic

--- lilliput-core/src/header.rs
+++ lilliput-core/src/header.rs
@@ -141,3 +141,54 @@ impl Header {
         }
     }
 }
+
+#[cfg(any(test, feature = "testing"))]
+impl proptest::prelude::Arbitrary for Header {
+    type Parameters = ();
+    type Strategy = proptest::strategy::BoxedStrategy<Self>;
+
+    fn arbitrary_with(_args: Self::Parameters) -> Self::Strategy {
+        use proptest::strategy::Strategy;
+        proptest::prop_oneof![
+            IntHeader::arbitrary().prop_map(Self::from),
+            StringHeader::arbitrary().prop_map(Self::from),
+            SeqHeader::arbitrary().prop_map(Self::from),
+            MapHeader::arbitrary().prop_map(Self::from),
+            FloatHeader::arbitrary().prop_map(Self::from),
+            BytesHeader::arbitrary().prop_map(Self::from),
+            BoolHeader::arbitrary().prop_map(Self::from),
+            UnitHeader::arbitrary().prop_map(Self::from),
+            NullHeader::arbitrary().prop_map(Self::from),
+        ]
+        .boxed()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use proptest::prelude::*;
+
+    use crate::{
+        config::EncodingConfig,
+        decoder::Decoder,
+        encoder::Encoder,
+        io::{SliceReader, VecWriter},
+    };
+
+    use super::*;
+
+    proptest! {
+        #[test]
+        fn encode_decode_roundtrip(header in Header::arbitrary(), config in EncodingConfig::arbitrary()) {
+            let mut encoded: Vec<u8> = Vec::new();
+            let writer = VecWriter::new(&mut encoded);
+            let mut encoder = Encoder::new(writer, config);
+            encoder.encode_header(&header).unwrap();
+
+            let reader = SliceReader::new(&encoded);
+            let mut decoder = Decoder::new(reader);
+            let decoded = decoder.decode_header().unwrap();
+            prop_assert_eq!(&decoded, &header);
+        }
+    }
+}
