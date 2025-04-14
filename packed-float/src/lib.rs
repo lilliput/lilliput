fb9dedc4fb2fd647c6049c67af0d845762b882e1
Vincent Esche <regexident@gmail.com>
2025-04-14 12:31:36 +0200

    Add `trait FpClassify` and corresponding implementations for each packed float type

--- packed-float/src/lib.rs
+++ packed-float/src/lib.rs
@@ -1,5 +1,6 @@
 mod be_bytes;
 mod bits;
+mod classify;
 mod cmp;
 mod floats;
 mod native;
@@ -7,6 +8,7 @@ mod repr;
 
 pub use self::be_bytes::{FpFromBeBytes, FpToBeBytes};
 pub use self::bits::{FpFromBits, FpToBits};
+pub use self::classify::FpClassify;
 pub use self::floats::{F16, F24, F32, F40, F48, F56, F64, F8};
 pub use self::repr::FpRepr;
 
