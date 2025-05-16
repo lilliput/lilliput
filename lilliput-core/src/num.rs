db44cdc7b0d6dc40fb6aa0ad10d1cacb907d59b3
Vincent Esche <regexident@gmail.com>
2025-05-16 21:32:53 +0200

    Refine item visibilities in core crate

--- lilliput-core/src/num.rs
+++ lilliput-core/src/num.rs
@@ -1,10 +1,10 @@
-pub(crate) mod float;
-pub(crate) mod int;
+pub mod float;
+pub mod int;
 
 mod be_bytes;
 mod int_cast;
 mod zigzag;
 
-pub(crate) use self::be_bytes::{WithBeBytes, WithPackedBeBytes, WithValidatedPackedBeBytes};
-pub(crate) use self::int_cast::{TryFromInt, TryIntoInt};
-pub(crate) use self::zigzag::{FromZigZag, ToZigZag};
+pub use self::be_bytes::{WithBeBytes, WithPackedBeBytes, WithValidatedPackedBeBytes};
+pub use self::int_cast::{TryFromInt, TryIntoInt};
+pub use self::zigzag::{FromZigZag, ToZigZag};
